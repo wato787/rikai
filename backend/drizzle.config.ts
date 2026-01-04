31f655b9ce9d3ba3ecaa283643db3741d2c921c7
kimura <shibaduke4@gmail.com>
2026-01-04 18:13:26 +0900

    chore: DB設定と依存追加。コマンドmise db:studio調整

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -4,5 +4,7 @@ export default defineConfig({
   schema: "./src/db/schema.ts", // スキーマファイルの場所
   out: "./drizzle", // SQLファイルの出力先
   dialect: "sqlite", // D1はSQLiteベースなのでsqliteを指定
-  driver: "d1-http",
+  dbCredentials: {
+    url: ".wrangler/state/v3/d1/miniflare-D1DatabaseObject/4b0a8873fcb9b73b46869d12a4aa2caa5ce8fda261c88d436cfa5e0e5918c82f.sqlite",
+  },
 });
