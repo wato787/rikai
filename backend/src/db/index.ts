a75dee5ba049ea4c8cc524a0863c7db1cc427da2
kimura <shibaduke4@gmail.com>
2026-01-04 18:19:30 +0900

    lintエラー修正

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,5 +1,5 @@
-import { drizzle, DrizzleD1Database } from 'drizzle-orm/d1';
-import * as schema from './schema';
+import { drizzle, DrizzleD1Database } from "drizzle-orm/d1";
+import * as schema from "./schema";
 
 export const getDb = (d1: DrizzleD1Database) => {
   return drizzle(d1, { schema });
