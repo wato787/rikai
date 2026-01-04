5ac8b0549f4f4fc3546637156d27663dd402ab4e
kimura <shibaduke4@gmail.com>
2026-01-04 18:33:16 +0900

    chore: 環境変数とDB設定更新

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
   out: "./drizzle", // SQLファイルの出力先
   dialect: "sqlite", // D1はSQLiteベースなのでsqliteを指定
   dbCredentials: {
-    url: ".wrangler/state/v3/d1/miniflare-D1DatabaseObject/4b0a8873fcb9b73b46869d12a4aa2caa5ce8fda261c88d436cfa5e0e5918c82f.sqlite",
+    url: process.env.DATABASE_URL || "src/db/local.db",
   },
 });
